2b07ecf23025a4f2adfd6d7669765a95b81fb9ce
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-07-12 15:21:47 +0300

    create class app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Counter from './components/Counter';
+// import './App.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <>
+      {Counter/}
+        <h1>Please leave feedback</h1>;<button type="button">Good</button>
+        <button type="button">Neutral</button>
+        <button type="button">Bad</button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good:</li>
+          <li>Neutral:</li>
+          <li>Bad:</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default App;
